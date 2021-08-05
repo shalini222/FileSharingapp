e24754aafe1781c45d5180e6339d4a3a44fc46c2
Shalini Dutta <Shaliniduttaofficial@gmail.com>
2021-08-06 02:02:30 +0530

    Updates

---
+++ precache-manifest.5e0077b5f6cd2aa583580ab1e5de75c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccbb12bc75451b288f31fac986fd564a",
+    "url": "/FileSharingapp/index.html"
+  },
+  {
+    "revision": "189f654b700a688a943d",
+    "url": "/FileSharingapp/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "822e8bcb649daf020cc8",
+    "url": "/FileSharingapp/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "189f654b700a688a943d",
+    "url": "/FileSharingapp/static/js/2.0b865d9b.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "/FileSharingapp/static/js/2.0b865d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822e8bcb649daf020cc8",
+    "url": "/FileSharingapp/static/js/main.793028ad.chunk.js"
+  },
+  {
+    "revision": "2a1a8678531448587ae2",
+    "url": "/FileSharingapp/static/js/runtime-main.f478e182.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "/FileSharingapp/static/media/box.df8b84d5.png"
+  }
+]);
